c32d5ce9ee66368100741e2bcb75537c3f2f0888
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-10-07 06:26:54 +0000

    updated

--- mean/public/js/controllers/header.js
+++ mean/public/js/controllers/header.js
@@ -2,10 +2,10 @@ angular.module('mean.system').controller('HeaderController', ['$scope', 'Global'
     $scope.global = Global;
 
     $scope.menu = [{
-        "title": "Articles",
-        "link": "articles"
+        "title": "Bookmarks",
+        "link": "bookmarks"
     }, {
-        "title": "Create New Article",
-        "link": "articles/create"
+        "title": "Create New Bookmark",
+        "link": "bookmark/create"
     }];
 }]);
