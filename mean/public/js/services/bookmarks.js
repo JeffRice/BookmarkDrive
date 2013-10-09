9b20d095971cbbe877fa0a8e55f3a51c7bbcb0e4
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-10-09 04:48:44 +0000

    updated bookmark service

--- mean/public/js/services/bookmarks.js
+++ mean/public/js/services/bookmarks.js
@@ -1,6 +1,6 @@
 //Bookmarks service used for bookmarks REST endpoint
 angular.module('mean.bookmarks').factory("Bookmarks", ['$resource', function($resource) {
-    return $resource('bookmarks/:articleId', {
+    return $resource('bookmarks/:bookmarkId', {
         bookmarkId: '@_id'
     }, {
         update: {
