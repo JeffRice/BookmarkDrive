0dc7a7996caa17134eb23e86ad8cd904614bd2c8
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-10-19 06:00:56 +0000

    added bookmark objects for testing

--- mean/app/controllers/index.js
+++ mean/app/controllers/index.js
@@ -3,11 +3,110 @@
  */
 var mongoose = require('mongoose'),
     async = require('async'),
+    Bookmark = mongoose.model('Bookmark'),
     _ = require('underscore');
 
 
 exports.render = function(req, res) {
     res.render('index', {
         user: req.user ? JSON.stringify(req.user) : "null"
+    });
+};
+
+/**
+* Find bookmark by id
+*/
+exports.bookmark = function(req, res, next, id) {
+    Bookmark.load(id, function(err, bookmark) {
+        if (err) return next(err);
+        if (!bookmark) return next(new Error('Failed to load bookmark ' + id));
+        req.bookmark = bookmark;
+        next();
+    });
+};
+
+/**
+* Create a bookmark
+*/
+exports.create = function(req, res) {
+    var bookmark = new Bookmark(req.body);
+    bookmark.user = req.user;
+
+    bookmark.save(function(err) {
+        if (err) {
+            return res.send('users/signup', {
+                errors: err.errors,
+                bookmark: bookmark
+            });
+        } else {
+            res.jsonp(bookmark);
+        }
+    });
+};
+
+/**
+* Update a bookmark
+*/
+exports.update = function(req, res) {
+    var bookmark = req.bookmark;
+
+    bookmark = _.extend(bookmark, req.body);
+
+    bookmark.save(function(err) {
+        res.jsonp(bookmark);
+    });
+};
+
+/**
+* Delete a bookmark
+*/
+exports.destroy = function(req, res) {
+    var bookmark = req.bookmark;
+
+    bookmark.remove(function(err) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(bookmark);
+        }
+    });
+};
+
+/**
+* Show a bookmark
+*/
+exports.show = function(req, res) {
+    res.jsonp(req.bookmark);
+};
+
+/**
+* List of Bookmarks
+*/
+exports.all = function(req, res){
+    Bookmark.find().sort('-created').populate('user', 'name username').exec(function(err, bookmarks) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(bookmarks);
+        }
+    });
+};
+
+/**
+* List of Personal Bookmarks
+*/
+exports.all = function(req, res) {
+    Bookmark.find({ "category" : "Search engines" }).sort('-created').populate('user', 'name username').exec(function(err, mybookmarks) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(mybookmarks);
+        }
     });
 };
