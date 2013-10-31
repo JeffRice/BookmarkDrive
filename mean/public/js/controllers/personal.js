ea63fd1dda8a8b97aa0e721984be22ebd6b900db
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-10-31 23:54:32 +0000

    trying to work with the returned bookmarks object

--- mean/public/js/controllers/personal.js
+++ mean/public/js/controllers/personal.js
@@ -1,3 +1,15 @@
-angular.module('mean.system').controller('IndexController', ['$scope', 'Bookmarks', 'Global', function ($scope, Global, Bookmarks) {
+angular.module('mean.bookmarks').controller('PersonalController', ['$scope', '$routeParams', '$location', 'Global', 'Bookmarks', function ($scope, $routeParams, $location, Global, Bookmarks) {
     $scope.global = Global;
+
+    Bookmarks.find({ title: 'Dino RIders' }, function (err, docs) {
+  if (err) return handleErrorSomehow(err)
+  console.log(Array.isArray(docs)) // true
+  docs.forEach(function (doc) {
+    console.log(typeof doc.save) // function
+  })
+})
+        
+
+
 }]);
+
