6607a5884801bc86a70901aec620a0e4e7592158
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-11-12 16:41:16 +0000

    added validation module to app

--- mean/public/js/app.js
+++ mean/public/js/app.js
@@ -1,4 +1,4 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.bookmarks', 'ui.unique']);
+window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.bookmarks', 'ui.unique', 'ui.validate']);
 
 angular.module('mean.system', []);
 angular.module('mean.bookmarks', []);
